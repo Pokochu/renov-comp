83d1fe6d0ae491458f758f4704037d1f4d38bcaa
NB-GPOGONYI\gpogonyi <gpogonyi@chemaxon.com>
2022-02-22 14:19:18 +0100

    CHG structure calculator test

--- src/test/java/com/test/renov/calculator/SurfaceCalculatorTest.java
+++ src/test/java/com/test/renov/calculator/SurfaceCalculatorTest.java
@@ -32,13 +32,23 @@ public class SurfaceCalculatorTest {
 
     @Test(dataProvider = "testRooms")
     public void shouldReturnTheCorrectSideSizeofTheRoom(Room room, int expectedSideSize) {
+        //given
+
+        //when call calculateSmallestSide
         int smallestSide = testCalculator.calculateSmallestSide(room);
+
+        //then it should return the correct size
         assertEquals(smallestSide, expectedSideSize);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void shouldThrowExceptionOnWrongInput() {
+        //given
+
+        //when calculateSmallestSidewith invalid Room
         int smallestSide = testCalculator.calculateSmallestSide(new Room(0, 1, 1));
+
+        //then should throw Exception
     }
 
     @DataProvider()
@@ -53,19 +63,27 @@ public class SurfaceCalculatorTest {
 
     @Test(dataProvider = "testRoomsForSurfaceCalculations")
     public void shouldReturnTheCorrectSurfaceSizeofTheRoom(Room room, int expectedSurfaceSize) {
+        //given
+
+        //when calculateRoomSurface
         int surfaceSize = testCalculator.calculateRoomSurface(room);
+
+        //then should return correct surface size of the room
         assertEquals(surfaceSize, expectedSurfaceSize);
     }
 
     @Test
     public void shouldReturnTheCorrectSumOfSurfacesForRooms() {
+        //given test dataset of rooms
         Map<Room, Integer> rooms = new HashMap<>();
         rooms.put(new Room(1, 1, 1), 2);
         rooms.put(new Room(2, 2 , 3), 1);
         rooms.put(new Room(3, 2, 1), 3);
 
+        //when call calculateAllSurface
         int allSurface = testCalculator.calculateAllSurface(rooms);
 
+        //then should return the sum of all room surface sizes and room's smallest side sizes
         assertEquals(allSurface, 122);
     }
 }
